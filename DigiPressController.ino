79f6623767dabe8851d405b92fda02b728706043
Robert Lester <robertlesterjr@mac.com>
2022-01-18 10:16:51 -0500

    ..

--- DigiPressController.ino
+++ DigiPressController.ino
@@ -1,9 +1,12 @@
 
 
-#define LED_PIN 12
+#define LED_PIN 13
+#define SWITCH_PIN 12
 
 #define MAX_PRESSURE_POT A0
 #define MAX_DIFF_POT     A1
+
+#define ON_PIN_STATE HIGH
 
 #define MAX_PSI 5
 #define MAX_DIFF 2
@@ -13,7 +16,9 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, OUTPUT);
   pinMode(MAX_PRESSURE_POT, INPUT);
+  pinMode(MAX_DIFF_POT, INPUT);
 
   Serial.begin(9600);
 
@@ -27,10 +32,14 @@ void loop() {
   float psi = readPSI();
   float diff = readDiff();
 
+  
+
   Serial.print("Max PSI: ");
   Serial.print(psi);
   Serial.print("  Diff: ");
   Serial.println(diff);
+
+  //toggleRelay();
 }
 
 float readPSI() {
@@ -45,4 +54,24 @@ float readDiff() {
 
   float psi = (potLevel * MAX_DIFF) / 1023;
   return psi;
+}
+
+void toggleRelay() {
+  digitalWrite(SWITCH_PIN, !powerState());
+}
+
+void toggleRelayOn() {
+  digitalWrite(SWITCH_PIN, ON_PIN_STATE);
+}
+
+void toggleRelayOff() {
+  digitalWrite(SWITCH_PIN, !ON_PIN_STATE);
+}
+
+bool powerState() {
+  return digitalRead(SWITCH_PIN);
+}
+
+void readPressure() {
+
 }
